f74d227a0e1cff9f38b0d12879f9bd4bfa82ac32
doromiert <doromiert@gmail.com>
2026-01-24 14:04:04 +0100

    zero font works fine

--- pkgs/zero-font/package.nix
+++ pkgs/zero-font/package.nix
@@ -10,40 +10,46 @@ stdenv.mkDerivation {
 
   src = ./src;
 
-  raw = ./src/resources/zero-src;
-
   nativeBuildInputs = with pkgs; [
     fontforge
     python3
   ];
 
-  buildPhase = "
+  # Pass paths to the script via environment variables as it expects
+  # We use '.' because we 'cd' into the source directory or reference relative to build root
+  rawPath = "./resources/zero-src";
+  # condensedPath = "./resources/zero-condensed-src"; # Uncomment if you have this
+
+  buildPhase = ''
     runHook preBuild
 
-    cp $src/make-zero.py ./build.py
-    mkdir -p share/fonts/truetype
+    # Create a local output directory that is writable
+    mkdir -p ./dist
 
-    echo \"Generating Zero font...\"
-    out=. fontforge -script ./build.py
+    echo "Generating Zero font..."
+
+    # We set 'out' to our local dist folder so the script can write to it
+    # We use 'rawPath' which we defined above
+    env out=./dist rawPath=$rawPath fontforge -script ./scripts/make-zero.py
 
     runHook postBuild
-  ";
+  '';
 
-  installPhase = "
+  installPhase = ''
     runHook preInstall
 
+    # Now we move files from the local 'dist' to the actual Nix $out
     mkdir -p $out/share/fonts/truetype
-    install -Dm644 share/fonts/truetype/*.ttf -t $out/share/fonts/truetype/
+    install -Dm644 ./dist/share/fonts/truetype/*.ttf -t $out/share/fonts/truetype/
 
     runHook postInstall
-  ";
+  '';
 
   meta = with lib; {
     description = "Zero Font";
-    homepage = "https://zenos.neg-zero.com"; # i know this doesn't exist yet but uhhhh
-    license = licenses.napl;
+    homepage = "https://zenos.neg-zero.com";
+    license = licenses.napl; # Updated to reflect your custom NAPL if defined elsewhere
     maintainers = with maintainers; [ doromiert ];
     platforms = platforms.linux;
   };
-
 }
