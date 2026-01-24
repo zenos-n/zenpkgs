eb77c01a9e97824d48db78d2ad7294d1948d797b
doromiert <doromiert@gmail.com>
2026-01-24 21:44:49 +0100

    swisstag

--- pkgs/programs/swisstag/package.nix
+++ pkgs/programs/swisstag/package.nix
@@ -1,30 +1,120 @@
 {
   lib,
   stdenv,
-  pkgs,
-  ...
+  fetchFromGitHub,
+  python3,
+  chromaprint,
+  makeWrapper,
 }:
-stdenv.mkDerivation {
+
+let
+  pyPkgs = python3.pkgs;
+
+  lyricsgenius = pyPkgs.buildPythonPackage rec {
+    pname = "lyricsgenius";
+    version = "3.7.5";
+    pyproject = true;
+
+    src = pyPkgs.fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-xPEMFPeYBFXGXIfQz9EIa+CWba7K+ZaTAy02tlo4qhY=";
+    };
+
+    doCheck = false;
+    nativeBuildInputs = with pyPkgs; [
+      setuptools
+      hatchling
+    ];
+    propagatedBuildInputs = with pyPkgs; [
+      requests
+      beautifulsoup4
+      hatchling
+    ];
+  };
+
+  syncedlyrics = pyPkgs.buildPythonPackage rec {
+    pname = "syncedlyrics";
+    version = "1.0.0";
+    pyproject = true;
+
+    src = pyPkgs.fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-JrwIR7s1tYAyS3eYCsmxzkHJz/7WOe7rkDABfhumuZE=";
+    };
+
+    doCheck = false;
+    nativeBuildInputs = with pyPkgs; [ poetry-core ];
+    propagatedBuildInputs = with pyPkgs; [
+      requests
+      beautifulsoup4
+      rapidfuzz
+    ];
+  };
+
+  pythonEnv = python3.withPackages (
+    ps: with ps; [
+      mutagen
+      musicbrainzngs
+      thefuzz
+      requests
+      unidecode
+      pillow
+      beautifulsoup4
+      rapidfuzz
+      syncedlyrics
+      lyricsgenius
+    ]
+  );
+in
+stdenv.mkDerivation rec {
   pname = "swisstag";
-  version = "1.0";
+  version = "5.2";
 
-  src = ./src;
+  src = fetchFromGitHub {
+    owner = "doromiert";
+    repo = "swisstag";
+    rev = version;
+    sha256 = "sha256-/ttw8TfzXk9UrwHKZfuEALRUnYlUIq+b7bOrOC8bf6A=";
+  };
 
-  unpackPhase = " ";
-
-  nativeBuildInputs = with pkgs; [
-
+  nativeBuildInputs = [
+    makeWrapper
   ];
 
-  buildPhase = " ";
+  buildInputs = [
+    pythonEnv
+    chromaprint
+  ];
 
-  installPhase = " ";
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin $out/share/man/man1
+    cp swisstag.py $out/bin/swisstag
+    cp swisstag.1 $out/share/man/man1/swisstag.1
+
+    chmod +x $out/bin/swisstag
+
+    runHook postInstall
+  '';
+
+  postFixup = ''
+    # 1. Patch the shebang to use our constructed pythonEnv
+    sed -i "1s|^#!/usr/bin/env python3|#!${pythonEnv}/bin/python3|" $out/bin/swisstag
+
+    # 2. Wrap the binary to include fpcalc (from chromaprint) in the PATH
+    wrapProgram $out/bin/swisstag \
+      --prefix PATH : ${lib.makeBinPath [ chromaprint ]}
+  '';
 
   meta = with lib; {
     description = "Automatic music tagger and renamer";
     homepage = "https://zenos.neg-zero.com";
     license = licenses.napl;
     maintainers = with maintainers; [ doromiert ];
+    mainProgram = "swisstag";
     platforms = platforms.linux;
   };
 }
