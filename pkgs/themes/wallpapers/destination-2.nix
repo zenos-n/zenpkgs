94547c9746a831f21fe176b4341d979578a654d7
doromiert <doromiert@gmail.com>
2026-05-10 22:46:40 +0200

    proper dest2 wall support

--- pkgs/themes/wallpapers/destination-2.nix
+++ pkgs/themes/wallpapers/destination-2.nix
@@ -16,18 +16,63 @@ stdenv.mkDerivation {
     sha256 = "sha256-vNuSipOuRaZEMRS2oI5vI4TgkzpyYxYMQBDLIMzV9mM=";
   };
 
-  # we don't need to build anything, just move files
   dontBuild = true;
 
   installPhase = ''
-    mkdir -p $out/share/wallpapers/destination-2
-    cp $src/* $out/share/wallpapers/destination-2
+        mkdir -p $out/share/backgrounds/destination-2
+        mkdir -p $out/share/gnome-background-properties
+        mkdir -p $out/share/wallpapers
+
+        cat <<EOF > $out/share/gnome-background-properties/destination-2.xml
+    <?xml version="1.0"?>
+    <!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">
+    <wallpapers>
+    EOF
+
+        # Using find to handle the spaces in filenames (e.g., 'blue dark.png')
+        find . -maxdepth 1 -name "*.png" -type f | while read img; do
+          fn=$(basename "$img")
+
+          # Create a pretty name (e.g., "blue dark" -> "Blue Dark")
+          pretty_name=$(echo "''${fn%.*}" | sed 's/\b\(.\)/\u\1/g')
+          # Create a unique, filesystem-safe name for KDE folders
+          safe_name="Destination-2-$(echo "''${fn%.*}" | tr ' ' '-')"
+
+          cp "$img" "$out/share/backgrounds/destination-2/$fn"
+
+          cat <<EOF >> $out/share/gnome-background-properties/destination-2.xml
+      <static>
+        <name>Destination 2 ($pretty_name)</name>
+        <filename>$out/share/backgrounds/destination-2/$fn</filename>
+        <options>zoom</options>
+      </static>
+    EOF
+
+          # KDE Plasma requires a folder per wallpaper entry
+          kdir="$out/share/wallpapers/$safe_name"
+          mkdir -p "$kdir/contents/images"
+
+          # Use symlinks for KDE to save space (linking to the backgrounds folder)
+          ln -s "$out/share/backgrounds/destination-2/$fn" "$kdir/contents/images/$fn"
+
+          cat <<EOF > "$kdir/metadata.desktop"
+    [Desktop Entry]
+    Name=Destination 2 ($pretty_name)
+    X-KDE-PluginInfo-Name=$safe_name
+    X-KDE-PluginInfo-Author=doromiert
+    X-KDE-ServiceTypes=Wallpaper
+    Type=Service
+    EOF
+        done
+
+        echo "</wallpapers>" >> $out/share/gnome-background-properties/destination-2.xml
   '';
 
   meta = with lib; {
-    description = "Fastfetch theme for ZenOS";
+    description = "Destination 2 wallpaper pack by doromiert (GNOME/KDE Support)";
     homepage = "https://github.com/zenos-n/destination-2";
     license = licenses.napalm;
     maintainers = [ maintainers.doromiert ];
+    platforms = platforms.all;
   };
 }
