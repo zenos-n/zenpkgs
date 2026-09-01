4c93f80e05b3b2524d7b33d2305ab7341c22b812
doromiert <doromiert@gmail.com>
2026-09-01 21:12:24 +0200

    fix: initialize Home Manager users

--- nixos-modules/webapps.nix
+++ nixos-modules/webapps.nix
@@ -6,6 +6,7 @@
 
 let
   cfg = config.zenos.system.webApps;
+  normalUsers = lib.filterAttrs (_: user: user.isNormalUser) config.users.users;
   webAppModules = [
     ../hm-modules/webapps/module.nix
     ../hm-modules/webapps/firefox/module.nix
@@ -23,26 +24,33 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    assertions = lib.mapAttrsToList (name: _userConfig: {
-      assertion = builtins.hasAttr name config.users.users;
-      message = "zenos.system.webApps.users.${name} must name a declared system user";
-    }) cfg.users;
+  config = {
+    assertions = lib.optionals cfg.enable (
+      lib.mapAttrsToList (name: _userConfig: {
+        assertion = builtins.hasAttr name config.users.users;
+        message = "zenos.system.webApps.users.${name} must name a declared system user";
+      }) cfg.users
+    );
 
     home-manager = {
       useGlobalPkgs = true;
       useUserPackages = true;
-      users = lib.mapAttrs (name: webAppConfig: {
-        imports = webAppModules;
-        home = {
-          username = name;
-          homeDirectory = config.users.users.${name}.home;
-          stateVersion = config.system.stateVersion;
-        };
-        zenos.webApps = webAppConfig // {
-          enable = true;
-        };
-      }) cfg.users;
+      users = lib.mapAttrs (
+        name: user:
+        {
+          home = {
+            username = name;
+            homeDirectory = user.home;
+            stateVersion = config.system.stateVersion;
+          };
+        }
+        // lib.optionalAttrs (cfg.enable && builtins.hasAttr name cfg.users) {
+          imports = webAppModules;
+          zenos.webApps = cfg.users.${name} // {
+            enable = true;
+          };
+        }
+      ) normalUsers;
     };
   };
 }
