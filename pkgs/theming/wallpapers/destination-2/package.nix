765deaea8999949d65e2f47f6d87928b32bb770a
doromiert <doromiert@gmail.com>
2026-09-01 23:33:57 +0200

    feat: install Destination II GNOME catalog

--- pkgs/theming/wallpapers/destination-2/package.nix
+++ pkgs/theming/wallpapers/destination-2/package.nix
@@ -11,8 +11,8 @@ stdenvNoCC.mkDerivation {
   src = fetchFromGitHub {
     owner = "zenos-n";
     repo = "destination-2";
-    rev = "1.0.0";
-    hash = "sha256-vNuSipOuRaZEMRS2oI5vI4TgkzpyYxYMQBDLIMzV9mM=";
+    rev = "ae0bde2f214308325709c05f0f47af5d45eae9fe";
+    hash = "sha256-fNenryFtgqxM7ejF7QoO/HVSrj5Vb+HLyJX/WALrMsc=";
   };
 
   dontBuild = true;
@@ -22,53 +22,19 @@ stdenvNoCC.mkDerivation {
     backgrounds="$out/share/backgrounds/destination-2"
     properties="$out/share/gnome-background-properties/destination-2.xml"
     install -d "$backgrounds" "$(dirname "$properties")" "$out/share/wallpapers"
-
-    cat > "$properties" <<'EOF'
-    <?xml version="1.0"?>
-    <!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">
-    <wallpapers>
-    EOF
+    install -m 0644 ./*.png "$backgrounds/"
+    install -m 0644 destination-2.xml "$properties"
+    substituteInPlace "$properties" \
+      --replace-fail '/usr/share/backgrounds/destination-2' "$backgrounds"
 
     for dark_path in ./*\ dark.png; do
       color="$(basename "$dark_path" ' dark.png')"
-      light_path="./$color light.png"
       dark_name="$color dark.png"
-      light_name="$color light.png"
-      pretty="$(printf '%s' "$color" | sed 's/.*/\u&/')"
       safe="Destination-2-$color"
-      install -m 0644 "$dark_path" "$backgrounds/$dark_name"
-
-      if [ -f "$light_path" ]; then
-        install -m 0644 "$light_path" "$backgrounds/$light_name"
-        cat >> "$properties" <<EOF
-    <wallpaper deleted="false">
-      <name>Destination 2 ($pretty)</name>
-      <filename>$backgrounds/$light_name</filename>
-      <filename-dark>$backgrounds/$dark_name</filename-dark>
-      <options>zoom</options>
-      <shade_type>solid</shade_type>
-      <pcolor>#000000</pcolor>
-      <scolor>#000000</scolor>
-    </wallpaper>
-    EOF
-      else
-        cat >> "$properties" <<EOF
-    <wallpaper deleted="false">
-      <name>Destination 2 ($pretty)</name>
-      <filename>$backgrounds/$dark_name</filename>
-      <options>zoom</options>
-      <shade_type>solid</shade_type>
-      <pcolor>#000000</pcolor>
-      <scolor>#000000</scolor>
-    </wallpaper>
-    EOF
-      fi
 
       install -d "$out/share/wallpapers/$safe/contents/images"
       ln -s "$backgrounds/$dark_name" "$out/share/wallpapers/$safe/contents/images/$dark_name"
     done
-
-    echo '</wallpapers>' >> "$properties"
     runHook postInstall
   '';
 
