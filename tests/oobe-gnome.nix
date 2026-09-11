846c79c9a4d4dc82fb36892d5c7bd38c29dd97dd
doromiert <doromiert@gmail.com>
2026-09-11 22:33:09 +0200

    fix: preserve oobe system defaults

--- tests/oobe-gnome.nix
+++ tests/oobe-gnome.nix
@@ -100,6 +100,9 @@ assert lib.all
   ];
 assert temporary.systemd.user.services.zenos-oobe.environment.ZENOS_SETUP_DRY_RUN == "0";
 assert temporary.home-manager.users.zenos.dconf.settings == { };
+assert lib.elem temporary.systemd.package temporary.environment.systemPackages;
+assert lib.elem "/etc/dbus-1" temporary.environment.pathsToLink;
+assert lib.any (lib.hasPrefix "D /var/empty ") temporary.systemd.tmpfiles.rules;
 assert lib.hasInfix (builtins.unsafeDiscardStringContext (
   toString temporary.services.displayManager.sessionData.wrapper
 )) temporary.services.greetd.settings.initial_session.command;
